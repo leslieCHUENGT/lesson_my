e422bbc6a4ca3d612dacc044222a0ca18c9d6966
leslieCHUENGT <95614952+leslieCHUENGT@users.noreply.github.com>
2023-02-23 23:16:07 +0800

    map

--- 1.js
+++ 1.js
@@ -1,20 +1,35 @@
-// 再来看一下使用数组和set来做哈希法的局限。
-// 数组的大小是受限制的，而且如果元素很少，而哈希值太大会造成内存空间的浪费。
-// set是一个集合，里面放的元素只能是一个key，而两数之和这道题目，
-
-//不仅要判断y是否存在而且还要记录y的下标位置，因为要返回x 和 y的下标。所以set 也不能用。
-
-// 此时就要选择另一种数据结构：map ，map是一种key value的存储结构，可以用key保存数值，用value在保存数值所在的下标。
+// 如果 thisArg 参数提供给 map，则会被用作回调函数的 this 值。
+// 否则 undefined 会被用作回调函数的 this 值。
+// this 的值最终相对于 callbackFn 函数的可观察性是依据确定函数绑定的 this 的常见规则决定的。
 
 
-var twoSum = function (nums, target) {
-    let hash = {};
-    for (let i = 0; i < nums.length; i++){ // 遍历当前元素，并在map中寻找是否有匹配的key
-        if (hash[target - nums[i]] !== undefined) {
-            return [hash[target - nums[i]],i];
-        }
-        hash[nums[i]] = i;// 如果没找到匹配对，就把访问过的元素和下标加入到map中
+Array.prototype.map = function (callbackFn,thisArg) {
+    if (this === null || this === undefined) {//这个this是在原型链上添加的方法，一般不可能为null||undefined
+        //Array.protype.map.call(null)
+        throw new TypeError('cannot read proerty map of null undefined')
     }
-    return [];
+    if (Object.prototype.toString.call(callbackFn) != '[object Function]') {
+        throw new TypeError(callbackFn + 'is not a function')
+    }
+    let O = Object(this);
+    let T = thisArg;
+    let len = O.length;
+    let A = new Array(len);//全新分配内存
+    for (let k = 0; k < len; k++){
+        if (k in O) {
+            let kValue = O[k];
+            let mappedValue = callbackFn.call(T,kValue);
+            A[k] = mappedValue;
+        }
+    }
+    return A;
 }
-//当使用js写map哈希法，定义一个hash={}，hash[nums]=i即认为是hash map中的存储结构为 {key：数据元素，value：数组元素对应的下标}
+
+
+let obj = { val: 3 };
+let newNums = nums.map(function (item, index, array){
+    return item + index + array[index] + this.val;
+},obj)
+console.log(newNums);
+
+
