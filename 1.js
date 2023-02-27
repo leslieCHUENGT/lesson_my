462fe182b0d5ab738bba599fdb9b3df40ca237b8
leslieCHUENGT <95614952+leslieCHUENGT@users.noreply.github.com>
2023-02-27 14:06:33 +0800

    flatten
    flatten

--- 1.js
+++ 1.js
@@ -1,35 +1,25 @@
-// 如果 thisArg 参数提供给 map，则会被用作回调函数的 this 值。
-// 否则 undefined 会被用作回调函数的 this 值。
-// this 的值最终相对于 callbackFn 函数的可观察性是依据确定函数绑定的 this 的常见规则决定的。
+var arr = [1, [2, [3, 4]], 5];
+// 递归
 
-
-Array.prototype.map = function (callbackFn,thisArg) {
-    if (this === null || this === undefined) {//这个this是在原型链上添加的方法，一般不可能为null||undefined
-        //Array.protype.map.call(null)
-        throw new TypeError('cannot read proerty map of null undefined')
-    }
-    if (Object.prototype.toString.call(callbackFn) != '[object Function]') {
-        throw new TypeError(callbackFn + 'is not a function')
-    }
-    let O = Object(this);
-    let T = thisArg;
-    let len = O.length;
-    let A = new Array(len);//全新分配内存
-    for (let k = 0; k < len; k++){
-        if (k in O) {
-            let kValue = O[k];
-            let mappedValue = callbackFn.call(T,kValue);
-            A[k] = mappedValue;
+function flatten(arr) {
+    var result = [];
+    for (var i = 0, len = arr.length; i < len; i++) {
+        if (Array.isArray(arr[i])) {
+            result = result.concat(flatten(arr[i]))
+        } else {
+            result.push(arr[i]);
         }
     }
-    return A;
+    return result;
 }
 
+// [1,2,3,4,5]
+console.log(flatten(arr));
 
-let obj = { val: 3 };
-let newNums = nums.map(function (item, index, array){
-    return item + index + array[index] + this.val;
-},obj)
-console.log(newNums);
+// [1,2,3,4,5]
+console.log(flatten(arr));
 
-
+function flatten1(arr) {
+    return arr.toString().split(', ').map(item => +item)
+}
+console.log (flatten1(arr));
